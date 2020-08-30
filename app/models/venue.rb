76edd9e42c244bf5113c88d8faccc220752e1c4c
Eduardo <jeduardo824@gmail.com>
2020-08-30 18:59:50 -0300

    add methods to Venue to help on best seats finder

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -5,4 +5,17 @@ class Venue < ApplicationRecord
 
   validates :title, presence: true
   validates :rows, :columns, numericality: { only_integer: true, greater_than: 0 }
+
+  def center_seat
+    center = columns / 2.0
+    columns.even? ? center : center.ceil
+  end
+
+  def ordered_seats
+    if columns.even?
+      available_seats.order(:row, column: :desc)
+    else
+      available_seats.order(:row, :column)
+    end
+  end
 end
--- spec/models/venue_spec.rb
+++ spec/models/venue_spec.rb
@@ -3,7 +3,6 @@
 require "rails_helper"
 
 RSpec.describe Venue, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
   describe "has_many" do
     it { should have_many(:available_seats) }
   end
@@ -19,6 +18,50 @@ RSpec.describe Venue, type: :model do
     it do
       should validate_numericality_of(:columns).only_integer
                                                .is_greater_than(0)
+    end
+  end
+
+  describe "#center_seat" do
+    context "when number of columns is even" do
+      subject { create(:venue, columns: 4) }
+
+      it "returns half numbers of columns" do
+        expect(subject.center_seat).to eq(2)
+      end
+    end
+
+    context "when number of columns is not even" do
+      subject { create(:venue, columns: 5) }
+
+      it "returns the ceil value of the half number of columns" do
+        expect(subject.center_seat).to eq(3)
+      end
+    end
+  end
+
+  describe "#ordered_seats" do
+    context "when number of columns is even" do
+      let!(:first_seat) { create(:available_seat, venue: subject, row: 1, column: 3) }
+      let!(:second_seat) { create(:available_seat, venue: subject, row: 1, column: 4) }
+      let(:expected_seats_order) { [second_seat, first_seat] }
+
+      subject { create(:venue, rows: 1, columns: 4) }
+
+      it "returns half numbers of columns" do
+        expect(subject.ordered_seats).to eq(expected_seats_order)
+      end
+    end
+
+    context "when number of columns is odd" do
+      let!(:first_seat) { create(:available_seat, venue: subject, row: 1, column: 3) }
+      let!(:second_seat) { create(:available_seat, venue: subject, row: 1, column: 4) }
+      let(:expected_seats_order) { [first_seat, second_seat] }
+
+      subject { create(:venue, rows: 1, columns: 5) }
+
+      it "returns half numbers of columns" do
+        expect(subject.ordered_seats).to eq(expected_seats_order)
+      end
     end
   end
 end
